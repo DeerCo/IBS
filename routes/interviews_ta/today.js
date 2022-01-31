93a56785c98e615833726deeade7690c56070a31
Han Xian Xu Huang <howie010103@gmail.com>
2022-01-31 02:25:32 +0000

    Change all time to eastern time

--- routes/interviews_ta/today.js
+++ routes/interviews_ta/today.js
@@ -12,7 +12,7 @@ router.get("/:task/today", (req, res) => {
 		return;
 	}
 
-	let sql_times = "SELECT id, task, to_char(time AT time zone 'America/Toronto', 'YYYY-MM-DD HH24:MI') AS time, student, length, location, cancelled, note FROM interviews WHERE task = ($1) AND ta = ($2) AND time BETWEEN ($3) AND ($3) + INTERVAL '24 HOURS' ORDER BY time";
+	let sql_times = "SELECT id, task, to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI') AS time, student, length, location, cancelled, note FROM interviews WHERE task = ($1) AND ta = ($2) AND time BETWEEN ($3) AND ($3) + INTERVAL '24 HOURS' ORDER BY time";
 	client.query(sql_times, [req.params["task"], res.locals["ta"], moment().tz("America/Toronto").format("YYYY-MM-DD") + " America/Toronto"], (err, pgRes) => {
 		if (err) {
 			res.status(404).json({ message: "Unknown error." });
