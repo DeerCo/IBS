3c743baee8578a2e83469e65ae8c5358862c8584
Han Xian Xu Huang <howie010103@gmail.com>
2022-01-30 21:31:19 -0500

    Add order by time for backup

--- routes/interviews_ta/backup_task.js
+++ routes/interviews_ta/backup_task.js
@@ -4,7 +4,7 @@ const client = require("../../setup/db");
 const helpers = require("../../utilities/helpers");
 
 router.get("/:task/backup", (req, res) => {
-	let sql_backup = "SELECT id, task, CONCAT(to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI'), ' America/Toronto'), ta, student, length, location, cancelled, note FROM interviews WHERE task = ($1)";
+	let sql_backup = "SELECT id, task, CONCAT(to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI'), ' America/Toronto'), ta, student, length, location, cancelled, note FROM interviews WHERE task = ($1) ORDER BY time";
 	client.query(sql_backup, [req.params["task"]], (err, pgRes) => {
 		if (err) {
 			res.status(404).json({ message: "Unknown error." });
