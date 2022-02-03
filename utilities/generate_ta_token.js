54fc7d1b581b05f1c24e63ce8ab6d35844e0cf18
Han Xian Xu Huang <howie010103@gmail.com>
2022-02-03 18:17:14 -0500

    Change interviews task

--- utilities/generate_ta_token.js
+++ utilities/generate_ta_token.js
@@ -1,4 +1,10 @@
 require("dotenv").config({ path: "../.env" });
 
 const helpers = require("./helpers");
-console.log(helpers.generateTaAccessToken("Howie", "phase_2"));
+console.log(helpers.generateTaAccessToken("Howie", "phase_1"));
+console.log(helpers.generateTaAccessToken("Lance", "phase_1"));
+console.log(helpers.generateTaAccessToken("Hans", "phase_1"));
+console.log(helpers.generateTaAccessToken("Rohan", "phase_1"));
+console.log(helpers.generateTaAccessToken("Osama", "phase_1"));
+console.log(helpers.generateTaAccessToken("Wencai", "phase_1"));
+console.log(helpers.generateTaAccessToken("Kianoosh", "phase_1"));
