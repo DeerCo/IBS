56ff9d51d791ccbaf709a66c3b0d1308bf42d761
Han Xian Xu Huang <howie010103@gmail.com>
2022-03-03 23:08:37 -0500

    Open a3

--- routes/a3.js
+++ routes/a3.js
@@ -4,9 +4,9 @@ const registerRouter = require("./a3/register");
 const textDataRouter = require("./a3/text_data");
 const textLikesRouter = require("./a3/text_likes");
 
-router.use("/", function (req, res, next) {
-	// next();
-	res.sendStatus(403);
+router.use("/", function(req, res, next) {
+    next();
+    // res.sendStatus(403);
 })
 
 router.use("/", registerRouter);
