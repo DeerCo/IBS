d1a386eb19c25268c1ae6a9727c94f52085691c8
Han Xian Xu Huang <howie010103@gmail.com>
2022-03-06 21:22:29 -0500

    Send confirmation emails to all group members

--- routes/interviews/middleware.js
+++ routes/interviews/middleware.js
@@ -2,28 +2,29 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-router.use("/:task", function (req, res, next) {
-	const authHeader = req.headers["authorization"];
-	const token = authHeader && authHeader.split(" ")[1];
-	if (req.params["task"] === "token") {
-		res.status(400).json({ message: "Task is invalid." });
-	} else if (token == null) {
-		res.status(401).json({ message: "You need to provide a valid token." });
-	} else {
-		jwt.verify(token, process.env.TOKEN_SECRET, (err, group_data) => {
-			if (err) {
-				res.status(403).json({ message: "Your token is invalid. Please generate a new one." });
-			} else {
-				if (req.params["task"] === group_data["task"]) {
-					res.locals["group"] = group_data["group"];
-					res.locals["email"] = group_data["email"];
-					next();
-				} else {
-					res.status(400).json({ message: "Your token is invalid or is for another task." });
-				}
-			}
-		})
-	}
+router.use("/:task", function(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (req.params["task"] === "token") {
+        res.status(400).json({ message: "Task is invalid." });
+    } else if (token == null) {
+        res.status(401).json({ message: "You need to provide a valid token." });
+    } else {
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, group_data) => {
+            if (err) {
+                res.status(403).json({ message: "Your token is invalid. Please generate a new one." });
+            } else {
+                if (req.params["task"] === group_data["task"]) {
+                    res.locals["group"] = group_data["group"];
+                    res.locals["user_email"] = group_data["user_email"];
+                    res.locals["group_emails"] = group_data["group_emails"];
+                    next();
+                } else {
+                    res.status(400).json({ message: "Your token is invalid or is for another task." });
+                }
+            }
+        })
+    }
 })
 
 module.exports = router;
