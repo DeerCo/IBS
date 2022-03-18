cb9955869b483ad7dc091ac61224c2dae713b131
Han Xian Xu Huang <howie010103@gmail.com>
2022-03-17 22:31:08 -0400

    Open phase 2 interviews

--- utilities/generate_ta_token.js
+++ utilities/generate_ta_token.js
@@ -1,10 +1,10 @@
 require("dotenv").config({ path: "../.env" });
 
 const helpers = require("./helpers");
-console.log(helpers.generateTaAccessToken("Howie", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Lance", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Hans", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Rohan", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Osama", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Wencai", "phase_2_mentor_2"));
-console.log(helpers.generateTaAccessToken("Kianoosh", "phase_2_mentor_2"));
+console.log(helpers.generateTaAccessToken("Howie", "phase_2"));
+console.log(helpers.generateTaAccessToken("Lance", "phase_2"));
+console.log(helpers.generateTaAccessToken("Hans", "phase_2"));
+console.log(helpers.generateTaAccessToken("Rohan", "phase_2"));
+console.log(helpers.generateTaAccessToken("Osama", "phase_2"));
+console.log(helpers.generateTaAccessToken("Wencai", "phase_2"));
+console.log(helpers.generateTaAccessToken("Kianoosh", "phase_2"));
