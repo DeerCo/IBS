71ba05374e64cf0588f8dd732bd57e51563f461a
Han Xian Xu Huang <howie010103@gmail.com>
2022-03-28 18:19:10 -0400

    Allow talking to TA with canclled interview before

--- setup/constants.js
+++ setup/constants.js
@@ -12,8 +12,8 @@ module.exports = {
         "phase_3": { open_student: false, open_ta: true, length: 45, markus_id: 6, exclude: ["phase_1", "phase_2"] },
         "a2": { open_student: true, open_ta: true, length: 10, markus_id: 2, exclude: [] }
     },
-    sql_times: "SELECT to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI') AS time, COUNT(*) AS all_count, COUNT(student) AS booked_count, location FROM interviews WHERE task = ($1) AND ta != ALL(SELECT ta FROM interviews WHERE task = ANY($2) AND student = ($3)) GROUP BY time, location ORDER BY time",
+    sql_times: "SELECT to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI') AS time, COUNT(*) AS all_count, COUNT(student) AS booked_count, location FROM interviews WHERE task = ($1) AND ta != ALL(SELECT ta FROM interviews WHERE task = ANY($2) AND student = ($3) AND cancelled IS FALSE) GROUP BY time, location ORDER BY time",
     sql_check: "SELECT id, to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI') AS time, location FROM interviews WHERE student = ($1) AND task = ($2)",
-    sql_book: "UPDATE interviews SET student = ($1) WHERE id = (SELECT id FROM interviews WHERE task = ($2) AND time = ($3) AND student IS NULL AND location = ($4) AND ta != ALL(SELECT ta FROM interviews WHERE task = ANY($5) AND student = ($1)) LIMIT 1 FOR UPDATE) RETURNING location",
+    sql_book: "UPDATE interviews SET student = ($1) WHERE id = (SELECT id FROM interviews WHERE task = ($2) AND time = ($3) AND student IS NULL AND location = ($4) AND ta != ALL(SELECT ta FROM interviews WHERE task = ANY($5) AND student = ($1) AND cancelled IS FALSE) LIMIT 1 FOR UPDATE) RETURNING location",
     sql_cancel: "UPDATE interviews SET student = NULL WHERE id = ($1)"
 };
