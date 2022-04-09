2c49bfa6b6097c8615bce8e693301aa515cd667a
Han Xian Xu Huang <howie010103@gmail.com>
2022-04-08 23:38:41 -0400

    Open phase_3_mentor_2

--- setup/constants.js
+++ setup/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    rows_count: 40,
+    rows_count: 45,
     tasks: {
         "phase_1_mentor_1": { open_student: false, open_ta: true, length: 15, markus_id: 4, exclude: [] },
         "phase_1_mentor_2": { open_student: false, open_ta: true, length: 15, markus_id: 4, exclude: [] },
@@ -7,8 +7,8 @@ module.exports = {
         "phase_2_mentor_1": { open_student: false, open_ta: true, length: 15, markus_id: 5, exclude: [] },
         "phase_2_mentor_2": { open_student: false, open_ta: true, length: 15, markus_id: 5, exclude: [] },
         "phase_2": { open_student: true, open_ta: true, length: 60, markus_id: 5, exclude: ["phase_1", "phase_3"] },
-        "phase_3_mentor_1": { open_student: true, open_ta: true, length: 15, markus_id: 6, exclude: [] },
-        "phase_3_mentor_2": { open_student: false, open_ta: true, length: 15, markus_id: 6, exclude: [] },
+        "phase_3_mentor_1": { open_student: false, open_ta: true, length: 15, markus_id: 6, exclude: [] },
+        "phase_3_mentor_2": { open_student: true, open_ta: true, length: 15, markus_id: 6, exclude: [] },
         "phase_3": { open_student: false, open_ta: true, length: 45, markus_id: 6, exclude: ["phase_1", "phase_2"] },
         "a2": { open_student: true, open_ta: true, length: 10, markus_id: 2, exclude: [] },
         "a3": { open_student: true, open_ta: true, length: 10, markus_id: 3, exclude: [] }
--- utilities/generate_token_manually.js
+++ utilities/generate_token_manually.js
@@ -2,13 +2,13 @@ require("dotenv").config({ path: "../.env" });
 
 const helpers = require("./helpers");
 
-console.log(helpers.generateTaAccessToken("Howie", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Lance", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Hans", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Rohan", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Osama", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Wencai", "phase_3_mentor_1"));
-console.log(helpers.generateTaAccessToken("Kianoosh", "phase_3_mentor_1"));
+console.log(helpers.generateTaAccessToken("Howie", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Lance", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Hans", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Rohan", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Osama", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Wencai", "phase_3_mentor_2"));
+console.log(helpers.generateTaAccessToken("Kianoosh", "phase_3_mentor_2"));
 
 console.log(helpers.generateStudentAccessToken({ user_id: 0, user_name: "", email: "", type: "student" }));
 
