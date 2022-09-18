e5d6f4990f06c02a7aaf94993253150518161df1
Han Xian Xu Huang <howie010103@gmail.com>
2022-09-18 17:15:39 +0000

    Change attachment fieldname

--- routes/student/a1/submit.js
+++ routes/student/a1/submit.js
@@ -13,7 +13,7 @@ router.post("/submit", rate_limit.upload_limiter, upload.any(), (req, res) => {
         res.status(400).json({ message: "Something is wrong!" })
     } else if (!("feedback" in req.body)) {
         res.status(400).json({ message: "Something is wrong!" })
-    } else if (!("files" in req) || req.files.length !== 1 || (req.files[0]["fieldname"] != "attachment") || !("originalname" in req.files[0])) {
+    } else if (!("files" in req) || req.files.length !== 1 || (req.files[0]["fieldname"] != "attachments") || !("originalname" in req.files[0])) {
         res.status(400).json({ message: "Something is wrong!" })
     } else if (!("consent" in req.body)) {
         res.status(400).json({ message: "Something is wrong!" })
