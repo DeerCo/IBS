f539ba7741708c7f009f04b42f3a5e5201f5865c
Han Xian Xu Huang <howie010103@gmail.com>
2022-09-22 14:37:41 +0000

    Add ability to take down the server

--- app.js
+++ app.js
@@ -7,12 +7,27 @@ app.set('trust proxy', 1); // get the real ip address
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Take down the server
+app.use(function (req, res, next) {
+    let server_down = false;
+    
+    if (server_down){
+        if (req.ip !== "127.0.0.1" && req.ip.substring(7) !== "127.0.0.1"){
+            res.status(503).json({message: "We are making the server better! Please try again later."});
+            return;
+        }
+    }
+    
+    next();
+});
+
+// Handle requests with form-data
 const multer = require('multer');
 var upload = multer({});
 
-// Handle all requests with form-data here
 app.use(function (req, res, next) {
-    if (req.url === "/admin/marks/upload" || req.url === "/a1/submit") {
+    let url_allow_upload = ["/admin/marks/upload", "/a1/submit/"];
+    if (url_allow_upload.includes(req.url)) {
         next();
     } else {
         let cb = upload.none();
@@ -26,15 +41,12 @@ app.use(cors());
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-
+// Routes
+const rate_limit = require("./setup/rate_limit");
 const studentsRouter = require('./routes/student');
 const interviewsTaRouter = require('./routes/ta');
 const adminsRouter = require('./routes/admin');
-
-const rate_limit = require("./setup/rate_limit");
-
-// Routes 
+ 
 app.get("/", rate_limit.general_limiter, (req, res) => {
     res.status(418).json({ message: "Why are you here??? LOL --Howie" });
 })
@@ -46,17 +58,24 @@ app.use('/admin', adminsRouter);
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) {
         if (err.code === "LIMIT_FILE_SIZE") {
-            res.status(400).json({ message: "File is too large." });
+            res.status(406).json({ message: "File is too large." });
+        } else if (err.code === "LIMIT_UNEXPECTED_FILE") {
+            res.status(400).json({ message: "Unexpected file is uploaded. Please check if your URL is correct." });
         } else {
-            console.error(err.stack);
-            res.status(400).json({ message: "Upload fails." });
+            console.error(err);
+            console.log(req);
+            res.status(501).json({ message: "Upload fails. Please check error log." });
         }
+    } else if (err instanceof SyntaxError) {
+        return res.status(400).send({ message: "Invalid data is received." });
     } else {
-        console.error(err.stack);
+        console.error(err);
         res.status(500).json({ message: "Unknown error. Please check error log." });
     }
 })
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+    console.log(`The server is listening at http://localhost:${port}`)
 })
