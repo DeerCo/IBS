eab09ba54ca411ce1755c67ff435bbe55edb1db0
Han Xian Xu Huang <hanxianxu.huang@utoronto.ca>
2022-10-13 21:18:45 +0000

    Remove unused tables from db schema

--- backend/utilities/db.sql
+++ backend/utilities/db.sql
@@ -1,55 +1,8 @@
-CREATE TABLE interviews
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 0 MINVALUE 0 ),
-    task character varying(50) NOT NULL,
-    "time" timestamp with time zone NOT NULL,
-    ta character varying(50) NOT NULL,
-    student character varying(50),
-    length integer,
-    location character varying DEFAULT 'Zoom',
-    cancelled boolean NOT NULL DEFAULT false,
-    note character varying,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE ta
-(
-    username character varying NOT NULL,
-    password character varying NOT NULL,
-    PRIMARY KEY (username)
-);
-
-CREATE TABLE marks
-(
-    student character varying NOT NULL,
-    task character varying NOT NULL,
-    criteria character varying NOT NULL,
-    mark numeric,
-    total numeric,
-    description character varying,
-    PRIMARY KEY (student, task, criteria)
-);
-
-CREATE TABLE ddah
-(
-    username character varying NOT NULL,
-    duty character varying NOT NULL,
-    "time" integer NOT NULL,
-    supervisor_prepared timestamp with time zone,
-    chair_approved timestamp with time zone,
-    ta_accepted timestamp with time zone,
-    PRIMARY KEY (username, duty),
-    CONSTRAINT username FOREIGN KEY (username)
-        REFERENCES ta (username) MATCH SIMPLE
-        ON UPDATE RESTRICT
-        ON DELETE RESTRICT
-        NOT VALID
-);
-
 CREATE TABLE user_info
 (
     username character varying NOT NULL,
     password character varying NOT NULL,
+    email character varying NOT NULL,
     PRIMARY KEY (username)
 );
 
@@ -75,6 +28,47 @@ CREATE TABLE course_role
         NOT VALID,
     CONSTRAINT course_id FOREIGN KEY (course_id)
         REFERENCES course (course_id) MATCH SIMPLE
+        ON UPDATE RESTRICT
+        ON DELETE RESTRICT
+        NOT VALID
+);
+
+CREATE TABLE interviews
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 0 MINVALUE 0 ),
+    task character varying(50) NOT NULL,
+    "time" timestamp with time zone NOT NULL,
+    ta character varying(50) NOT NULL,
+    student character varying(50),
+    length integer,
+    location character varying DEFAULT 'Zoom',
+    cancelled boolean NOT NULL DEFAULT false,
+    note character varying,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE marks
+(
+    student character varying NOT NULL,
+    task character varying NOT NULL,
+    criteria character varying NOT NULL,
+    mark numeric,
+    total numeric,
+    description character varying,
+    PRIMARY KEY (student, task, criteria)
+);
+
+CREATE TABLE ddah
+(
+    username character varying NOT NULL,
+    duty character varying NOT NULL,
+    "time" integer NOT NULL,
+    supervisor_prepared timestamp with time zone,
+    chair_approved timestamp with time zone,
+    ta_accepted timestamp with time zone,
+    PRIMARY KEY (username, duty),
+    CONSTRAINT username FOREIGN KEY (username)
+        REFERENCES user_info (username) MATCH SIMPLE
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
         NOT VALID
