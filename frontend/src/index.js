6b2cd2bd7399b8f3abde619d72498663bf551a67
Han Xian Xu Huang <hanxianxu.huang@utoronto.ca>
2022-12-23 00:49:31 +0000

    Delete unused stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,6 @@ import Frist from "./components/student_first_page";
 import Tasks from "./components/student_tasks_page";
 import Files from "./components/student_file_page";
 import Interview from "./components/student_interview_page";
-import reportWebVitals from './reportWebVitals';
 import {createRoot} from 'react-dom/client';
 
 import MarkPage from "./components/Page/MarkPage";
@@ -27,8 +26,3 @@ root.render(
     </Routes>
   </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
