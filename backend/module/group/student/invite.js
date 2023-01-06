7684a37b6dd69bc6aedc9fad4cc77c63ec4891c8
Han Xian Xu Huang <hanxianxu.huang@utoronto.ca>
2023-01-06 07:26:27 +0000

    Limit max members in a group

--- backend/module/group/student/invite.js
+++ backend/module/group/student/invite.js
@@ -13,43 +13,54 @@ router.post("/", (req, res) => {
         return;
     }
 
-	let sql_select_user = "SELECT * FROM course_" + res.locals["course_id"] + ".group_user WHERE group_id = ($1) AND username = ($2) AND status = 'confirmed'";
-	let sql_select_group = "SELECT * FROM course_" + res.locals["course_id"] + ".group WHERE group_id = ($1)";
+	let sql_select_users = "SELECT * FROM course_" + res.locals["course_id"] + ".group_user WHERE group_id = ($1)";
+	let sql_select_max_member = "SELECT max_member FROM course_" + res.locals["course_id"] + ".task WHERE task = ($1)";
 	let sql_invite = "INSERT INTO course_" + res.locals["course_id"] + ".group_user (task, username, group_id, status) VALUES (($1), ($2), ($3), 'pending')";
 
-	client.query(sql_select_user, [req.body["group_id"], res.locals["username"]], (err, pgRes) => {
-		if (err) {
+	client.query(sql_select_users, [req.body["group_id"]], (err_select_users, pg_res_select_users) => {
+		if (err_select_users) {
 			res.status(404).json({ message: "Unknown error." });
-			console.log(err);
-			return;
-		} else if (pgRes.rowCount !== 1) {
-			res.status(403).json({ message: "You don't have access to invite." });
 			return;
 		}
 
-		client.query(sql_select_group, [req.body["group_id"]], (err, pgRes) => {
-			if (err) {
-				res.status(404).json({ message: "Unknown error." });
-				console.log(err);
-			} else if (pgRes.rowCount === 1) {
-				let task = pgRes.rows[0]["task"];
-				client.query(sql_invite, [task, req.body["username"], req.body["group_id"]], (err, pgRes) => {
-					if (err) {
-						if (err.code === "23503" && err.constraint === "username") {
-							res.status(400).json({ message: "The username is not found in the database." });
-						} else if (err.code === "23505") {
-							res.status(409).json({ message: "The user has joined a group or been invited by another group." });
-						} else {
-							res.status(404).json({ message: "Unknown error." });
-							console.log(err);
-						}
-					} else if (pgRes.rowCount === 1) {
-						res.status(200).json({ message: "User has been invited." });
-					}
-				});
-			} else {
-				res.status(400).json({ message: "The group id is not found in the database." });
+		let has_access = false;
+		for (let row of pg_res_select_users.rows){
+			if (row["username"] === res.locals["username"] && row["status"] === "confirmed"){
+				has_access = true;
 			}
+		}
+		if (!has_access){
+			res.status(400).json({ message: "You don't have access to invite." });
+			return;
+		}
+
+		let task = pg_res_select_users.rows[0]["task"];
+		client.query(sql_select_max_member, [task], (err_max_member, pg_res_max_member) => {
+			if (err_max_member) {
+				res.status(404).json({ message: "Unknown error." });
+				return;
+			}
+
+			let max_member = pg_res_max_member.rows[0]["max_member"];
+			if (pg_res_select_users.rowCount >= max_member){
+				res.status(400).json({ message: "No more user can be invited as the maximum has been reached." });
+				return;
+			}
+
+			client.query(sql_invite, [task, req.body["username"], req.body["group_id"]], (err_invite, pg_res_invite) => {
+				if (err_invite) {
+					if (err_invite.code === "23503" && err_invite.constraint === "username") {
+						res.status(400).json({ message: "The username is not found in the database." });
+					} else if (err_invite.code === "23505") {
+						res.status(409).json({ message: "The user has joined a group or been invited by another group." });
+					} else {
+						res.status(404).json({ message: "Unknown error." });
+						console.log(err_invite);
+					}
+				} else {
+					res.status(200).json({ message: "User has been invited." });
+				}
+			});
 		});
 	});
 })
