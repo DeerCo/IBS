40d0a4b8d525fde0413d08c43d26590d0c4828da
Han Xian Xu Huang <howie010103@gmail.com>
2023-03-15 23:20:44 -0400

    Return note in student booked interview

--- backend/module/interview/student/check.js
+++ backend/module/interview/student/check.js
@@ -27,7 +27,7 @@ router.get("/", (req, res) => {
 
         let sql_check = "SELECT interview_id, to_char(time AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI:SS') AS start_time, " +
             "to_char(time AT TIME ZONE 'America/Toronto' + CONCAT(length,' minutes')::INTERVAL, 'YYYY-MM-DD HH24:MI:SS') AS end_time, " +
-            "location FROM course_" + res.locals["course_id"] + ".interview WHERE group_id = ($1) AND task = ($2) AND cancelled = false";
+            "location, note FROM course_" + res.locals["course_id"] + ".interview WHERE group_id = ($1) AND task = ($2) AND cancelled = false";
         client.query(sql_check, [group_id, res.locals["task"]], (err, pgRes) => {
             if (err) {
                 res.status(404).json({ message: "Unknown error." });
@@ -36,7 +36,7 @@ router.get("/", (req, res) => {
                     res.status(200).json({ message: "You don't have a booked interview for " + res.locals["task"] + " yet.", booked: false });
                 } else {
                     let message = "Your interview for " + res.locals["task"] + " is from " + pgRes.rows[0]["start_time"] + " to " + pgRes.rows[0]["end_time"] + ". The location is " + pgRes.rows[0]["location"] + ".";
-                    res.status(200).json({ message: message, booked: true, start_time: pgRes.rows[0]["start_time"], end_time: pgRes.rows[0]["end_time"], location: pgRes.rows[0]["location"] });
+                    res.status(200).json({ message: message, booked: true, start_time: pgRes.rows[0]["start_time"], end_time: pgRes.rows[0]["end_time"], location: pgRes.rows[0]["location"], note: pgRes.rows[0]["note"] });
                 }
             }
         });
