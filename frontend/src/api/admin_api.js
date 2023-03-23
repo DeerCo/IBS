6f32852e21ce101a16d99a8a0f7e3cad8f051d27
Daksh <daksh3451@gmail.com>
2023-03-23 16:07:47 -0400

    admin page

--- frontend/src/api/admin_api.js
+++ frontend/src/api/admin_api.js
@@ -14,21 +14,21 @@ let all_courses = async () => {
   }
 }
 
-let add_course = async (course_code, course_session, gitlab_group_id, default_token_count, token_length, hidden) => {
+let add_course = async (data) => {
   let token = sessionStorage.getItem("token");
 
   let config = {
     headers: { Authorization: `Bearer ${token}` },
   };
 
-  let data = {
-    course_code: course_code,
-    course_session: course_session,
-    gitlab_group_id: gitlab_group_id,
-    default_token_count: default_token_count,
-    token_length: token_length,
-    hidden: hidden
-  }
+  // let data = {
+  //   course_code: course_code,
+  //   course_session: course_session,
+  //   gitlab_group_id: gitlab_group_id,
+  //   default_token_count: default_token_count,
+  //   token_length: token_length,
+  //   hidden: hidden
+  // }
 
   try {
     return await axios.post(process.env.REACT_APP_API_URL + "/admin/course/add", data, config);
@@ -37,22 +37,22 @@ let add_course = async (course_code, course_session, gitlab_group_id, default_to
   }
 }
 
-let change_course = async (course_id, course_code, course_session, gitlab_group_id, default_token_count, token_length, hidden) => {
+let change_course = async (data) => {
   let token = sessionStorage.getItem("token");
 
   let config = {
     headers: { Authorization: `Bearer ${token}` },
   };
 
-  let data = {
-    course_id: course_id,
-    course_code: course_code,
-    course_session: course_session,
-    gitlab_group_id: gitlab_group_id,
-    default_token_count: default_token_count,
-    token_length: token_length,
-    hidden: hidden
-  }
+  // let data = {
+  //   course_id: course_id,
+  //   course_code: course_code,
+  //   course_session: course_session,
+  //   gitlab_group_id: gitlab_group_id,
+  //   default_token_count: default_token_count,
+  //   token_length: token_length,
+  //   hidden: hidden
+  // }
 
   try {
     return await axios.put(process.env.REACT_APP_API_URL + "/admin/course/change", data, config);
@@ -61,10 +61,56 @@ let change_course = async (course_id, course_code, course_session, gitlab_group_
   }
 }
 
+let get_role = async (username) => {
+  let token = sessionStorage.getItem("token");
+
+  let config = {
+    headers: { Authorization: `Bearer ${token}` },
+  }
+
+  try {
+    return await axios.get(process.env.REACT_APP_API_URL + "/admin/role/get?username=" + username, config);
+  } catch (err) {
+    return err.response;
+  }
+}
+
+let add_role = async (data) => {
+  let token = sessionStorage.getItem("token");
+
+  let config = {
+    headers: { Authorization: `Bearer ${token}` },
+  }
+
+  try {
+    return await axios.post(process.env.REACT_APP_API_URL + "/admin/role/add", data, config);
+  } catch (err) {
+    return err.response;
+  }
+}
+
+let delete_role = async (data) => {
+  let token = sessionStorage.getItem("token");
+
+  let config = {
+    headers: { Authorization: `Bearer ${token}` },
+    data: data
+  }
+
+  try {
+    return await axios.delete(process.env.REACT_APP_API_URL + "/admin/role/delete", config);
+  } catch (err) {
+    return err.response;
+  }
+}
+
 let AdminApi = {
   all_courses,
   add_course,
-  change_course
+  change_course,
+  get_role,
+  add_role,
+  delete_role
 }
 
 export default AdminApi;
