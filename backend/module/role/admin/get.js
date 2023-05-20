8b89a27b50383fb74b26fc3949c495246961090e
Han Xian Xu Huang <howie010103@gmail.com>
2023-05-20 00:49:33 +0000

    Add feature to get all users for instructor and ta

--- backend/module/role/admin/get.js
+++ backend/module/role/admin/get.js
@@ -5,10 +5,10 @@ const helpers = require("../../../utilities/helpers");
 
 router.get("/", (req, res) => {
     if ("username" in req.query && !helpers.name_validate(req.query["username"])) {
-        var sql_role = "SELECT * FROM course_role WHERE username = ($1)";
+        var sql_role = "SELECT course_role.username, email, role, course_id FROM course_role LEFT JOIN user_info ON course_role.username = user_info.username WHERE course_role.username = ($1)";
         var sql_role_data = [req.query["username"]];
     } else if ("course_id" in req.query && !helpers.number_validate(req.query["course_id"])) {
-        var sql_role = "SELECT * FROM course_role WHERE course_id = ($1)";
+        var sql_role = "SELECT course_role.username, email, role, course_id FROM course_role LEFT JOIN user_info ON course_role.username = user_info.username WHERE course_id = ($1)";
         var sql_role_data = [req.query["course_id"]];
     } else {
         res.status(400).json({ message: "A valid username or course id must be provided." });
