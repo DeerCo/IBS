f1d018493848ed99b59e0f69870bea64d5b97ee5
Eric Ryu <ericryu19@gmail.com>
2023-05-22 16:22:13 -0400

    use flexy tabs component

--- frontend/src/components/Page/Instructor/AggregatedGrades.jsx
+++ frontend/src/components/Page/Instructor/AggregatedGrades.jsx
@@ -1,6 +1,8 @@
 import AggregatedGradesTable from '../../General/AggregatedGradesTable/AggregatedGradesTable';
 import Grid from '@mui/material/Unstable_Grid2';
 import NavBar from '../../Module/Navigation/NavBar';
+import FlexyTabs from '../../General/FlexyTabs/FlexyTabs';
+import { Typography } from '@mui/material';
 
 const rows = [
     {
@@ -45,6 +47,45 @@ const headCells = [
     }
 ];
 
+const flexyTabs = [
+    {
+        tabName: 'Tab 1',
+        tabId: 0,
+        tabSubheading: 'Example of Subheading 1',
+        tabContext: (
+            <Typography
+                color="textSecondary"
+                sx={{
+                    mt: 4
+                }}
+            >
+                Example of description 1
+            </Typography>
+        )
+    },
+    {
+        tabName: 'Tab 2',
+        tabId: 1,
+        tabSubheading: 'Example of Subheading 2',
+        tabContext: (
+            <Typography
+                color="textSecondary"
+                sx={{
+                    mt: 4
+                }}
+            >
+                Example of description 2
+            </Typography>
+        )
+    },
+    {
+        tabName: 'Aggregated Grades',
+        tabId: 2,
+        tabSubheading: 'Aggregated Grades',
+        tabContext: <AggregatedGradesTable rows={rows} headCells={headCells} />
+    }
+];
+
 const AggregatedGrades = (props) => {
     return (
         <Grid container spacing={2}>
@@ -52,7 +93,7 @@ const AggregatedGrades = (props) => {
                 <NavBar />
             </Grid>
             <Grid xs={12}>
-                <AggregatedGradesTable rows={rows} headCells={headCells} />
+                <FlexyTabs tabs={flexyTabs} />
             </Grid>
         </Grid>
     );
