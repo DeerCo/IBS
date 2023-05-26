9902004577a32c9755e18b373d78a0dd4cff23bb
joshuamenezes <joshua.menezes@outlook.com>
2023-05-26 01:15:51 -0400

    Get all groups per task per course

--- frontend/src/components/Page/Instructor/AssignmentGroupListPage.jsx
+++ frontend/src/components/Page/Instructor/AssignmentGroupListPage.jsx
@@ -4,11 +4,49 @@ import { toast } from 'react-toastify';
 import InstructorApi from '../../../api/instructor_api';
 import { Grid } from '@mui/material';
 
+
 const AssignmentGroupListPage = () => {
 
+    // How to get all courses an instructor takes??
+    // Will this require an overhaul of the backend?
+    const instructorCourses = [1];
+
+    // Get all groups per task per course
+    // {"courseId1": [Group 1, Group 2, ...]}
+    let groupsPerTaskPerCourse = {}
+    instructorCourses.forEach(course => {
+        let tasksPerCourse = InstructorApi.all_tasks(course);
+
+        let courseId = course.toString()
+
+        groupsPerTaskPerCourse[courseId] = [];
+        let allGroupsPerTask = tasksPerCourse.map(task => {
+            return InstructorApi.all_groups(course, task);
+        })
+        groupsPerTaskPerCourse[courseId].push(allGroupsPerTask);
+    })
+
     return (
-        <Grid>
-            
+        <Grid container
+              direction="column"
+              height="100%"
+              wrap="nowrap">
+          <NavBar item page="Home"/>
+          <Grid item container
+                spacing={2}
+                direction="row"
+                justifyContent="center"
+                alignContent="center"
+                justify="center"
+                flex="1 1 auto">
+            {roles.map((data, index) => (
+              <Grid item key={index}>
+                <Homecard data={data}/>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
-    )
+      );
 }
+
+export default AssignmentGroupListPage;
