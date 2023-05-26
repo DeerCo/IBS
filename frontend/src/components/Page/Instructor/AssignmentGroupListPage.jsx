d5cdb05258a0c0d1d5854cfb13b72d074e15f2bf
joshuamenezes <joshua.menezes@outlook.com>
2023-05-26 00:36:17 -0400

    Create assignment group list page

---
+++ frontend/src/components/Page/Instructor/AssignmentGroupListPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import InstructorApi from '../../../api/instructor_api';
+import { Grid } from '@mui/material';
+
+const AssignmentGroupListPage = () => {
+
+    return (
+        <Grid>
+            
+        </Grid>
+    )
+}
