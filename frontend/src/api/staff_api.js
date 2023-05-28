985e2c0ee0cc0a4aff81e64df035206f99727cf3
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2023-05-28 00:12:58 -0400

    completed the call to the backend to fetch roles of students in a course

--- frontend/src/api/staff_api.js
+++ frontend/src/api/staff_api.js
@@ -2,19 +2,31 @@ import axios from 'axios';
 
 // Accessible to any of TA/Instructor/Admin
 
-let get_students_in_course = async (course_id) => {
+/**
+ *
+ * @param courseId
+ * @returns {Promise<axios.AxiosResponse<any>|*>}
+ */
+let get_students_in_course = async (courseId) => {
     let token = sessionStorage.getItem('token');
 
     let config = {
         headers: { Authorization: `Bearer ${token}` }
     };
 
+    const role = findRoleInCourse(courseId);
+    let url;
+    if (role === 'admin') {
+        url = `${process.env.REACT_APP_API_URL}/admin/role/get?course_id=${courseId}`;
+    } else if (role === 'instructor') {
+        url = `${process.env.REACT_APP_API_URL}/instructor/course/${courseId}/role/get`;
+    } else {
+        // in this case role === 'ta'
+        url = `${process.env.REACT_APP_API_URL}/ta/course/${courseId}/role/get`;
+    }
+
     try {
-        // TODO: Endpoint missing
-        return await axios.get(
-            process.env.REACT_APP_API_URL + 'TODO: FIGURE OUT THE ENDPOINT',
-            config
-        );
+        return await axios.get(url, config);
     } catch (err) {
         return err.response;
     }
@@ -23,6 +35,33 @@ let get_students_in_course = async (course_id) => {
 // Accessible to any of Instructor/Admin
 
 // Accessible to any of TA/Admin
+
+// Helpers
+export const findRoleInCourse = (courseId) => {
+    const rolesStr = sessionStorage.getItem('roles');
+    if (!rolesStr) return null;
+
+    const roles = JSON.parse(rolesStr);
+
+    for (const course of roles) {
+        if (parseInt(course.course_id) === parseInt(courseId)) return course.role;
+    }
+
+    return null;
+};
+
+export const findCourseCodeInCourse = (courseId) => {
+    const rolesStr = sessionStorage.getItem('roles');
+    if (!rolesStr) return null;
+
+    const roles = JSON.parse(rolesStr);
+
+    for (const course of roles) {
+        if (parseInt(course.course_id) === parseInt(courseId)) return course.course_code;
+    }
+
+    return null;
+};
 
 const StaffApi = {
     get_students_in_course
