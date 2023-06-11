f73c339533912e60cecfd4f74fc74f406c59c6e7
Eric Ryu <ericryu19@gmail.com>
2023-06-11 15:50:11 -0400

    restore backend rate limiting

--- backend/setup/rate_limit.js
+++ backend/setup/rate_limit.js
@@ -1,13 +1,13 @@
 const rateLimit = require("express-rate-limit");
 
 const general_limiter = rateLimit({
-  max: 10000000,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: "You sent too many requests! Try again in one hour!",
 });
 
 const email_limiter = rateLimit({
-  max: 20000,
+  max: 20,
   windowMs: 60 * 60 * 1000,
   message: "You sent too many requests! Try again in one hour!",
 });
