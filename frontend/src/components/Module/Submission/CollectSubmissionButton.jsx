15a2c6c675865aca851abe14e95ee23de008c560
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2023-07-02 19:40:24 -0400

    modified collection button to work for only a single group

--- frontend/src/components/Module/Submission/CollectSubmissionButton.jsx
+++ frontend/src/components/Module/Submission/CollectSubmissionButton.jsx
@@ -3,12 +3,36 @@ import React from 'react';
 import StaffApi from '../../../api/staff_api';
 import { toast } from 'react-toastify';
 
-const CollectSubmissionButton = ({ course_id, task }) => {
+/**
+ * @param course_id the course to which the task belongs
+ * @param task the id/name of the task to collect submissions for
+ * @param group_id optional, if present will collect submission for the group with the given id
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const CollectSubmissionButton = ({ course_id, task, group_id }) => {
     const handleCollectSubmissions = () => {
+        if (typeof group_id !== 'undefined') {
+            return StaffApi.collectOneSubmission(course_id, group_id, true)
+                .then(() => {
+                    toast.success(
+                        `Submissions for ${task} successfully collected. Click Submissions > Download to download them.`,
+                        {
+                            theme: 'colored'
+                        }
+                    );
+                })
+                .catch((err) => {
+                    toast.error('Something went wrong. Try again', {
+                        theme: 'colored'
+                    });
+                });
+        }
+
         StaffApi.collectAllSubmissionsForTask(course_id, task, true)
-            .then(() => {
+            .then((res) => {
                 toast.success(
-                    `Submissions for ${task} successfully collected. Click Submissions > Download to download them.`,
+                    `${res.data.result.collected_count} submissions for ${task} collected. Click Submissions > Download to download them.`,
                     {
                         theme: 'colored'
                     }
