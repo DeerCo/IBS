b2bdcd28e0065265ed5c8bcad54a27371cf9f8c3
Eric Ryu <ericryu19@gmail.com>
2023-07-03 13:32:38 -0400

    customize to use flexy tabs and setup only list of courses tab

--- frontend/src/components/Page/Admin/AdminPage.jsx
+++ frontend/src/components/Page/Admin/AdminPage.jsx
@@ -3,15 +3,37 @@ import AdminApi from '../../../api/admin_api';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import Homecard from '../../Module/Course/Homecard';
-import { Box, Button, Card, Grid, TextField, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import NavBar from '../../Module/Navigation/NavBar';
+import FlexyTabs from '../../General/FlexyTabs/FlexyTabs';
+import AdminAddCourse from '../../General/AdminPageComponents/AdminAddCourse';
+import AdminChangeCourse from '../../General/AdminPageComponents/AdminChangeCourse';
+import AdminGetRole from '../../General/AdminPageComponents/AdminGetRole';
+import { Box, Typography } from '@mui/material';
 
 const AdminPage = () => {
     const [courses, setCourses] = useState({});
     const [role, setRole] = useState({});
 
-    const { register: registerAdd, handleSubmit: handleAdd } = useForm();
-    const { register: registerChange, handleSubmit: handleChange } = useForm();
-    const { register: registerGetRole, handleSubmit: handleGetRole } = useForm();
+    const {
+        register: registerAdd,
+        formState: formStateAdd,
+        control: controlAdd,
+        handleSubmit: handleAdd
+    } = useForm();
+    const {
+        register: registerChange,
+        formState: formStateChange,
+        control: controlChange,
+        handleSubmit: handleChange
+    } = useForm();
+    const {
+        register: registerGetRole,
+        formState: formStateGetRole,
+        control: controlGetRole,
+        handleSubmit: handleGetRole
+    } = useForm();
+
     const [checked, setChecked] = useState(true);
 
     const addCourse = (data) => {
@@ -70,7 +92,7 @@ const AdminPage = () => {
         });
     }, []);
 
-    return (
+    const oldComponent = (
         <div style={{ margin: '0.5ch', justifyContent: 'left', textAlign: 'left' }}>
             <h1>Admin Page</h1>
             <Grid container spacing={2}>
@@ -138,6 +160,90 @@ const AdminPage = () => {
                 </form>
             </div>
         </div>
+    );
+
+    const CurrentCoursesComponent = () => {
+        return (
+            <Grid container columns={12} spacing={2}>
+                {courses?.map?.((data, index) => (
+                    <Grid item key={index}>
+                        <Homecard data={{ ...data, ['role']: 'admin' }} />
+                    </Grid>
+                ))}
+            </Grid>
+        );
+    };
+
+    const tabs = [
+        {
+            tabName: 'List of Courses',
+            tabId: 0,
+            tabSubheading: 'Current Courses',
+            tabContent: <CurrentCoursesComponent />
+        },
+        {
+            tabName: 'Add Courses',
+            tabId: 1,
+            tabSubheading: 'Add new courses',
+            tabContent: (
+                <AdminAddCourse
+                    useFormObject={{
+                        register: registerAdd,
+                        handleSubmit: handleAdd,
+                        control: controlAdd,
+                        formState: formStateAdd
+                    }}
+                    apiCall={addCourse}
+                />
+            )
+        },
+        {
+            tabName: 'Change Course',
+            tabId: 2,
+            tabSubheading: 'Change existing course details',
+            tabContent: (
+                <AdminChangeCourse
+                    useFormObject={{
+                        register: registerChange,
+                        handleSubmit: handleChange,
+                        control: controlChange,
+                        formState: formStateChange
+                    }}
+                    apiCall={changeCourse}
+                />
+            )
+        },
+        {
+            tabName: 'Get Role',
+            tabId: 3,
+            tabSubheading: 'Get role from username',
+            tabContent: (
+                <AdminGetRole
+                    useFormObject={{
+                        register: registerGetRole,
+                        handleSubmit: handleGetRole,
+                        control: controlGetRole,
+                        formState: formStateGetRole
+                    }}
+                    apiCall={getRole}
+                />
+            )
+        }
+    ];
+
+    return (
+        <Grid container>
+            <Grid xs={12}>
+                <NavBar page="Admin Panel" role="admin" />
+            </Grid>
+            <Grid xs={12} sx={{ mt: 3 }}>
+                <Grid container justifyContent="center" direction="column" alignItems="center">
+                    <Grid xs={12}>
+                        <FlexyTabs tabs={tabs} width={1600} height={1000} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 };
 
