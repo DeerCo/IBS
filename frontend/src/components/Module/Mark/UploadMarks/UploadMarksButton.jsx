c79744de41811fd2436968f4e89e3d175f53464e
joshuamenezes <joshua.menezes@outlook.com>
2023-07-26 23:41:56 -0400

    Handle errors

--- frontend/src/components/Module/Mark/UploadMarks/UploadMarksButton.jsx
+++ frontend/src/components/Module/Mark/UploadMarks/UploadMarksButton.jsx
@@ -3,13 +3,22 @@ import { Button } from '@mui/material';
 import Grid from '@mui/material';
 import { Form } from 'react-router-dom';
 import StaffApi from '../../../../api/staff_api';
+import { toast } from 'react-toastify';
 
 const UploadMarksButton = (courseId, taskId) => {
     const [file, setFile] = useState(null);
 
     const handleUpload = (event) => {
         setFile(event.target.files[0]);
-        StaffApi.uploadMarksCSV(courseId, file, taskId);
+        StaffApi.uploadMarksCSV(courseId, file, taskId).then((res) => {
+            if (!res || !('status' in res)) {
+                toast.error('There was an error uploading the file.', { theme: 'colored' });
+            } else if (res['status'] === 200) {
+                toast.success('Marks successfully uploaded.', { theme: 'colored' });
+            } else {
+                toast.error('Unknown Error', { theme: 'colored' });
+            }
+        });
     };
 
     return (
