46af560bbffd8dee6136a527cf29c030fda181bd
Eric Ryu <ericryu19@gmail.com>
2023-07-27 13:30:10 -0400

    finished reschedule interview component... just need to fix backend

--- frontend/src/components/General/InterviewComponents/RescheduleInterview/RescheduleInterview.jsx
+++ frontend/src/components/General/InterviewComponents/RescheduleInterview/RescheduleInterview.jsx
@@ -11,30 +11,80 @@ import {
     UpdatedFieldsProvider,
     UpdatedFieldsContext
 } from '../../../../contexts/RescheduleContexts/UpdatedFieldsContext';
-import { Button } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
+import { toast } from 'react-toastify';
+import StaffApi from '../../../../api/staff_api';
+import { useNavigate } from 'react-router-dom';
 
 const RescheduleInterview = (props) => {
-    const { filterInputFieldsObj, setFilterInputFieldsObj } = React.useContext(FilterFieldsContext);
-    const { updatedInfo, setUpdatedInfo } = React.useContext(UpdatedFieldsContext);
-
-    // change interview
-    const rescheduleInterview = (task, toNewFieldsObj, filterInputFieldsObj) => {
-        // TaApi.changeInterview(course_id, task).then((res) => {});
-    };
-
     return (
         <FilterFieldsProvider>
             <UpdatedFieldsProvider>
-                <Grid container columns={12}>
-                    <Grid xs={6}>
-                        <RescheduleFilterFields courseId={props.courseId} taskId={props.taskId} />
-                    </Grid>
-                    <Grid xs={6}>
-                        <RescheduleUpdatedFields courseId={props.courseId} taskId={props.taskId} />
-                    </Grid>
+                <RescheduleInterviewContent {...props} />
+            </UpdatedFieldsProvider>
+        </FilterFieldsProvider>
+    );
+};
+
+const RescheduleInterviewContent = (props) => {
+    const navigate = useNavigate();
+    const { filterFields, setFilterFields } = React.useContext(FilterFieldsContext);
+    const { updatedFields, setUpdatedFields } = React.useContext(UpdatedFieldsContext);
+
+    // change interview
+    const rescheduleInterview = (task, toNewFieldsObj, filterInputFieldsObj) => {
+        // remove keys with null or empty string values
+        let cleanedFilterFields = Object.fromEntries(
+            Object.entries(filterInputFieldsObj).filter(
+                ([key, value]) => value != null && value !== ''
+            )
+        );
+
+        let cleanedUpdatedFields = Object.fromEntries(
+            Object.entries(toNewFieldsObj).filter(([key, value]) => value != null && value !== '')
+        );
+        // DEV message
+        // toast.success('Everything is ok', { theme: 'colored' });
+        // console.log('=======================');
+        // console.log(cleanedFilterFields);
+        // console.log(cleanedUpdatedFields);
+        StaffApi.changeInterview(
+            props.courseId,
+            task,
+            cleanedUpdatedFields,
+            cleanedFilterFields
+        ).then((res) => {
+            if (!res || !('status' in res)) {
+                toast.error('Unknown error', { theme: 'colored' });
+                navigate('/login');
+            } else if (res.status === 200) {
+                toast.success(res.data.message, { theme: 'colored' });
+            } else if (res.status === 400 || res.status === 409) {
+                toast.warn(res.data.message, { theme: 'colored' });
+            } else if (res.status === 401 || res.status === 403) {
+                console.log(res);
+                toast.warn('You need to login again', { theme: 'colored' });
+                navigate('/login');
+            } else {
+                toast.error('Unknown error', { theme: 'colored' });
+                navigate('/login');
+            }
+        });
+    };
+
+    return (
+        <Grid container columns={12}>
+            <Grid xs={6}>
+                <RescheduleFilterFields courseId={props.courseId} taskId={props.taskId} />
+            </Grid>
+            <Grid xs={6}>
+                <RescheduleUpdatedFields courseId={props.courseId} taskId={props.taskId} />
+                <Box display="flex" justifyContent="flex-end" alignItems="flex-end" sx={{ mr: 4 }}>
                     <Button
                         onClick={() => {
-                            rescheduleInterview(props.taskId, updatedInfo, filterInputFieldsObj);
+                            console.log(updatedFields);
+                            console.log(filterFields);
+                            rescheduleInterview(props.taskId, updatedFields, filterFields);
                         }}
                         variant="contained"
                         size="large"
@@ -42,9 +92,9 @@ const RescheduleInterview = (props) => {
                     >
                         Confirm Change
                     </Button>
-                </Grid>
-            </UpdatedFieldsProvider>
-        </FilterFieldsProvider>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
