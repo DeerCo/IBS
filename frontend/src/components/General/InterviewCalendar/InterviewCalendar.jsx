c3a3e34638a4326b03d508af97e1d56298d2d57c
Eric Ryu <ericryu19@gmail.com>
2023-08-06 08:46:17 -0400

    fix prop usage such that it is mui-compatible

--- frontend/src/components/General/InterviewCalendar/InterviewCalendar.jsx
+++ frontend/src/components/General/InterviewCalendar/InterviewCalendar.jsx
@@ -14,11 +14,15 @@ const localizer = momentLocalizer(moment);
 
 /**
  * Calendar component for interview booking
+ * @param events Default events to prefill calendar with
+ * @param eventClickHandler Event handler for clicking on events
+ * @param selectSlotHandler Event handler for clicking on days (be it eventful or not)
+ * @param width Width of calendar
  * @param props see below PropTypes
  * @returns {JSX.Element}
  * @constructor
  */
-const InterviewCalendar = (props) => {
+const InterviewCalendar = ({ events, eventClickHandler, selectSlotHandler, width, ...props }) => {
     const eventColors = (event) => {
         if (event.color) {
             return { className: `event-${event.color}` };
@@ -27,18 +31,21 @@ const InterviewCalendar = (props) => {
     };
 
     return (
-        <Card sx={{ width: props.width }}>
+        <Card sx={{ width: width }}>
             <CardContent>
                 <Calendar
                     selectable
-                    events={props.events}
+                    events={events}
                     defaultView="month"
                     scrollToTime={new Date(1970, 1, 1, 6)}
                     defaultDate={new Date()}
                     localizer={localizer}
-                    style={{ height: 'calc(100vh - 350px)', width: props.width - 60 }}
-                    onSelectEvent={props.eventClickHandler}
-                    onSelectSlot={props.selectSlotHandler}
+                    style={{
+                        height: 'calc(100vh - 350px)',
+                        width: typeof width === 'number' ? width - 60 : `calc(${width}-60px)`
+                    }}
+                    onSelectEvent={eventClickHandler}
+                    onSelectSlot={selectSlotHandler}
                     eventPropGetter={(event) => eventColors(event)}
                     {...props}
                 />
@@ -48,14 +55,10 @@ const InterviewCalendar = (props) => {
 };
 
 InterviewCalendar.propTypes = {
-    // Default events to prefill calendar with
-    events: PropTypes.array,
-    // Event handler for clicking on events
-    eventClickHandler: PropTypes.func,
-    // Event handler for clicking on days (be it eventful or not)
-    selectSlotHandler: PropTypes.func,
-    // Width of calendar
-    width: PropTypes.number
+    events: PropTypes.array.isRequired,
+    eventClickHandler: PropTypes.func.isRequired,
+    selectSlotHandler: PropTypes.func.isRequired,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired
 };
 
 export default InterviewCalendar;
