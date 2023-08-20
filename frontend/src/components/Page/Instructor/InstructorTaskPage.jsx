9f62c8acefec9d34515fd49fe5cfd5f34ba5433b
Eric Ryu <ericryu19@gmail.com>
2023-08-20 10:25:17 -0400

    instructor task page should be called InstructorTaskPage

--- frontend/src/components/Page/Instructor/InstructorTaskPage.jsx
+++ frontend/src/components/Page/Instructor/InstructorTaskPage.jsx
@@ -1,53 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import NavBar from "../../Module/Navigation/NavBar";
-import Taskcard from "../../Module/Course/Taskcard";
+import NavBar from '../../Module/Navigation/NavBar';
+import Taskcard from '../../Module/Course/Taskcard';
 import { Grid } from '@mui/material';
 import PageContainer from '../../FlexyMainComponents/container/PageContainer';
-import InstructorApi from "../../../api/instructor_api";
+import InstructorApi from '../../../api/instructor_api';
 import useSWR from 'swr';
 
-let StudentTaskPage = () => {
-  let navigate = useNavigate();
-  let { course_id } = useParams();
-  let [mainTasks, setMainTasks] = useState([]);
+let InstructorTaskPage = () => {
+    let navigate = useNavigate();
+    let { course_id } = useParams();
+    let [mainTasks, setMainTasks] = useState([]);
 
+    const {
+        data: tasks,
+        isLoading,
+        error
+    } = useSWR('/task/all', () =>
+        InstructorApi.all_tasks(course_id).then((res) => res['data']['task'])
+    );
 
-  const { data: tasks, isLoading, error } = useSWR('/task/all', () =>
-    InstructorApi.all_tasks(course_id).then((res) => res["data"]["task"])
-  );
+    useEffect(() => {
+        if (isLoading) return;
+        if (error) navigate('/login');
+        setMainTasks(
+            tasks
+                .filter((task) => task.interview_group === null)
+                .map((task) => ({
+                    ...task,
+                    subtasks: tasks.filter((subtask) => subtask.interview_group === task.task)
+                }))
+        );
+    }, [course_id, navigate, tasks, isLoading, error]);
 
-  useEffect(() => {
-    if (isLoading) return;
-    if (error) navigate("/login");
-    setMainTasks(tasks.filter(task => task.interview_group === null).map(task => ({
-      ...task,
-      subtasks: tasks.filter(subtask => subtask.interview_group === task.task)
-    })));
-
-  }, [course_id, navigate, tasks, isLoading, error]);
-
-  return (
-    <PageContainer
-      title={`${course_id} Tasks`}
-      description={`Contains the tasks for the course '${course_id}'`}
-    >
-      <Grid container
-        direction="column"
-        height="100%"
-        wrap="nowrap">
-        <NavBar item page="Task" role='instructor' />
-        <Grid item container
-          padding={'32px'}>
-          {mainTasks.map((data, index) => (
-            <Taskcard key={index} data={data} course_id={course_id} role='instructor' />
-          ))}
-        </Grid>
-      </Grid>
-    </PageContainer >
-  )
+    return (
+        <PageContainer
+            title={`${course_id} Tasks`}
+            description={`Contains the tasks for the course '${course_id}'`}
+        >
+            <Grid container direction="column" height="100%" wrap="nowrap">
+                <NavBar item page="Task" role="instructor" />
+                <Grid item container padding={'32px'}>
+                    {mainTasks.map((data, index) => (
+                        <Taskcard key={index} data={data} course_id={course_id} role="instructor" />
+                    ))}
+                </Grid>
+            </Grid>
+        </PageContainer>
+    );
 };
 
-
-export default StudentTaskPage;
+export default InstructorTaskPage;
