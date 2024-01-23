501f86fa12daa52ae4b3f98c24168483cc63bd1c
hezhongq <71187784+hezhongq@users.noreply.github.com>
2024-01-23 11:59:51 -0500

    removed comment/logs

--- backend/models/index.js
+++ backend/models/index.js
@@ -19,7 +19,6 @@ fs
     );
   })
   .forEach(file => {
-    // const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     const model = require(path.join(__dirname, file));
     db[model.name] = model;
   });
--- backend/module/auth/admin/middleware.js
+++ backend/module/auth/admin/middleware.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 
 router.use("/", function (req, res, next) {
-    console.log("====admin middleware====");
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
